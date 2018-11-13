1d1e7a7a0b55179bef08c5e89c6cc696365dd733
nils407 <nils407@schule.bremen.de>
2018-11-13 09:58:53 +0100

    Cell commenting started

--- src/Cell.java
+++ src/Cell.java
@@ -1,23 +1,35 @@
 import java.awt.Color;
 import java.util.Random;
 
+// objects of class Cell are needed in GameOfWildlife
 public class Cell {
     private int age;
     private Color color;
     private boolean alive;
 
+    /**
+     * creates a dead Cell; color is black since it shouldn't be visible on grid
+     */
     Cell() {
         this.age = 0;
         this.color = Color.BLACK;
         this.alive = false;
     }
-
+    /**
+     * creates a newly born cell with a specific color
+     * @param color: Color
+     * @param alive: boolean
+     */
     Cell(Color color, boolean alive) {
         this.age = 0;
         this.color = color;
         this.alive = alive;
     }
 
+    /**
+     * the copy constructor simply copies a Cell object
+     * @param cell: Cell
+     */
     Cell(Cell cell) {
         this.age = cell.age;
         this.color = cell.color;
